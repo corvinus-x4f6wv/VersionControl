d96bc19890e738589ef8e6c6e7d8c12c3751e717
Barnabás Balogh <barnabas.balogh@stud.uni-corvinus.hu>
2021-10-18 12:41:59 +0200

    7. és 8. feladat

--- UserMaintenance/week05/Form1.Designer.cs
+++ UserMaintenance/week05/Form1.Designer.cs
@@ -38,21 +38,21 @@ namespace week05
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(58, 65);
+            this.dataGridView1.Location = new System.Drawing.Point(59, 65);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(654, 150);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(349, 65);
+            this.dataGridView2.Location = new System.Drawing.Point(59, 255);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.Size = new System.Drawing.Size(654, 150);
             this.dataGridView2.TabIndex = 1;
             // 
             // Form1
