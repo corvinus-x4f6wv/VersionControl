9fda45bfc3cf91663c76819d4f4d96dc8c49b095
Barnabás Balogh <barnabas.balogh@stud.uni-corvinus.hu>
2021-11-12 10:26:14 +0100

    3. feladat

--- UserMaintenance/08week/Form1.cs
+++ UserMaintenance/08week/Form1.cs
@@ -13,9 +13,57 @@ namespace _08week
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _ballFactory;
+
+        public BallFactory BallFactory
+        {
+            get { return _ballFactory; }
+            set { _ballFactory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            BallFactory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = BallFactory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
+        }
+
+        private void createTimer_Tick_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conveyorTimer_Tick_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
