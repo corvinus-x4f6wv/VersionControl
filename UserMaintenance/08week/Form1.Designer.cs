06b3b9dcd172b73dddfe0be67b371a28781379b1
Barnabás Balogh <barnabas.balogh@stud.uni-corvinus.hu>
2021-11-26 09:17:10 +0100

    feladat

--- UserMaintenance/08week/Form1.Designer.cs
+++ UserMaintenance/08week/Form1.Designer.cs
@@ -38,6 +38,7 @@ namespace _08week
             this.lblNext = new System.Windows.Forms.Label();
             this.btnPresent = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -113,11 +114,21 @@ namespace _08week
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(557, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnPresent);
             this.Controls.Add(this.lblNext);
@@ -141,6 +152,7 @@ namespace _08week
         private System.Windows.Forms.Label lblNext;
         private System.Windows.Forms.Button btnPresent;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
