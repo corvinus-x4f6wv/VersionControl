baa36d16e6bb855deadd646458850450fa14ff21
Barnabás Balogh <barnabas.balogh@stud.uni-corvinus.hu>
2021-11-30 15:30:39 +0100

    osztályok létrehozása, beolvasás és random szám generálás

---
+++ UserMaintenance/week09/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week09.Entities
+{
+    public class DeathProbability
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+
+        public double P { get; set; }
+    }
+}
